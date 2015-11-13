f07b5c4ec1df288fdc843b2eb8b69d4a4371ca7a
AlexBennett <alexanderbondbennett@gmail.com>
2015-11-13 09:06:51 -0500

    Removed unnecessary attr_readers from children classes

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -2,25 +2,12 @@ require_relative "house"
 require_relative "occupant"
 require_relative "capacity_module"
 
-module Capacity
-  def full?(max_capacity)
-    array.length >= max_capacity
-  end
-
- def add(object)
-    array << object
- end
-
-   def remove(object)
-     array.delete_if { |item| item.first_name == object.first_name && item.last_name == object.last_name }
-   end
- end
 
 class Apartment < House
 
   include Capacity
 
-  attr_reader :address, :city, :zip_code, :asking_price, :rent, :lease_start_date, :lease_end_date
+  attr_reader :rent, :lease_start_date, :lease_end_date
   attr_accessor :array
   def initialize(address, city = "Boston", zip_code = "02108", asking_price = "$400,000", rent, lease_start_date, lease_end_date)
     super(asking_price)
--- lib/house.rb
+++ lib/house.rb
@@ -1,6 +1,6 @@
 require_relative "dwelling"
 class House < Dwelling
-  attr_reader :address, :city, :zip_code, :asking_price
+  attr_reader :asking_price
   def initialize(address, city = "Boston", zip_code = "02108", asking_price = "$400,000")
     super(address, city, zip_code)
     @asking_price = asking_price
